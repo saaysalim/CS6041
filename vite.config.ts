9dfd030fab75f63440c6e82ae4f156d3521141da
Salim Saay <72768220+saaysalim@users.noreply.github.com>
2024-10-13 22:54:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { plugin } from 'postcss'
 
-// https://vitejs.dev/config/
-export default {
-  base: "/FirstProject/",
-  plugin: [react()],
-}
+export default defineConfig({
+  plugins: [react()],
+  base: '/MyPortfolio/',  // Adjust base URL if necessary
+  build: {
+    outDir: 'public',  // Output to public folder
+  },
+})
+
 
